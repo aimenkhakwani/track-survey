20d463afb70b9b1a9b63c1c75847260c38e4ba1a
Aimen Khakwani <aimen.khakwani@hotmail.com>
2016-08-12 10:50:40 -0700

    Add js to make the form submit and respond to the user

---
+++ js/scripts.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var ruby = 0;
+    var php = 0;
+    var jav = 0;
+    var result = "";
+
+    if ($("input:radio[name=q1]:checked").val() === "php"){
+      php += 1;
+    } else if ($("input:radio[name=q1]:checked").val() === "ruby"){
+      ruby += 1;
+    } else if ($("input:radio[name=q1]:checked").val() === "jav"){
+      jav+= 1;
+    }
+
+    if ($("input:radio[name=q2]:checked").val() === "php"){
+      php += 1;
+    } else if ($("input:radio[name=q2]:checked").val() === "ruby"){
+      ruby += 1;
+    } else if ($("input:radio[name=q2]:checked").val() === "jav"){
+      jav+= 1;
+    }
+
+    if ($("input:radio[name=q3]:checked").val() === "php"){
+      php += 1;
+    } else if ($("input:radio[name=q3]:checked").val() === "ruby"){
+      ruby += 1;
+    } else if ($("input:radio[name=q3]:checked").val() === "jav"){
+      jav+= 1;
+    }
+
+    if ($("input:radio[name=q4]:checked").val() === "php"){
+      php += 1;
+    } else if ($("input:radio[name=q4]:checked").val() === "ruby"){
+      ruby += 1;
+    } else if ($("input:radio[name=q4]:checked").val() === "jav"){
+      jav+= 1;
+    }
+
+    if ($("input:radio[name=q5]:checked").val() === "php"){
+      php += 1;
+    } else if ($("input:radio[name=q5]:checked").val() === "ruby"){
+      ruby += 1;
+    } else if ($("input:radio[name=q5]:checked").val() === "jav"){
+      jav+= 1;
+    }
+
+    if ($("input:radio[name=q6]:checked").val() === "php"){
+      php += 1;
+    } else if ($("input:radio[name=q6]:checked").val() === "ruby"){
+      ruby += 1;
+    } else if ($("input:radio[name=q6]:checked").val() === "jav"){
+      jav+= 1;
+    }
+
+    if ((ruby > jav) && (ruby > php)){
+      $(".pursue").text("Ruby");
+    } else if ((jav > ruby) && (jav > php)){
+      $(".pursue").text("Java");
+    } else if ((php > ruby) && (php > jav)){
+      $(".pursue").text("pHp");
+    } else {
+      $("#output").text("Youre Results were tied. We can't help you. Sorry!");
+    }
+    //alert("Ruby: " + ruby + " pHp: " + php + " Java: " + jav);
+    $("#output").show();
+
+  });
+
+
+});
